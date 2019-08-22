6797f50a00a651e5eb194c0856bc12db1563bb16
Gabriel Mandelli <gabriel-mandelli@hotmail.com>
2019-08-22 00:35:03 -0300

    Updates

---
+++ precache-manifest.5e9418fb7ebf49620c22675ef2322eb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e083dc9b15f1bfc6dcbcd7e778669df1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600ac0449634a124bff0",
+    "url": "/static/css/main.404f076b.chunk.css"
+  },
+  {
+    "revision": "7778a478317394d6c07f",
+    "url": "/static/js/2.774d7cf3.chunk.js"
+  },
+  {
+    "revision": "600ac0449634a124bff0",
+    "url": "/static/js/main.9f5ab251.chunk.js"
+  },
+  {
+    "revision": "9fbcf85c31ed2fce4db3",
+    "url": "/static/js/runtime~main.5a6d6b07.js"
+  },
+  {
+    "revision": "ebc891e59279b9be726b4139dfd137b2",
+    "url": "/static/media/like.ebc891e5.svg"
+  },
+  {
+    "revision": "08870d84b2a14dd2cc785b6663461fb3",
+    "url": "/static/media/woman-earrings-jewelry-makeup.08870d84.jpg"
+  }
+]);
